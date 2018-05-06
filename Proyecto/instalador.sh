65cc6f68015e01fb80369d7fd8e436ca85b0014e
Agustin Palmeira <agustin.palmeira@fdvsolutions.com>
2018-05-06 02:09:25 -0300

    - Chequeo de version de Perl que sea mayor o igual a la numero 5.

--- Proyecto/instalador.sh
+++ Proyecto/instalador.sh
@@ -1,7 +1,16 @@
-#perl -v
-MIN_PERL_VERSION=5
-PERL_VERSION=$(perl -e 'print $];')
-if [ "$PERL_VERSION" -ge "$MIN_PERL_VERSION" ]
-then
-  echo $PERL_VERSION
-fi 
+#!/bin/sh
+
+#Devuelve 0 en caso de que la version de Perl sea mayor o igual a la 5, 1 en caso contrario.
+function checkPerlVersion
+{
+  if [ $(perl -V:version | grep "='[5-9]" -c) -eq 1 ] || [ $(perl -V:version | grep "='..\." -c) -eq 1] ;
+  then
+     echo 'La version de Perl es valida.'
+     return 0
+  else
+     echo 'La version de Perl no es valida.' 
+     return 1
+  fi
+}
+
+
