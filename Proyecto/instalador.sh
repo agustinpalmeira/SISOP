40885f713d63fb623747f160625e80f016b81d98
Agustin Palmeira <agustin.palmeira@fdvsolutions.com>
2018-05-14 16:50:59 -0300

    - Crear todos los directorios usando los directorios default o los ingresados por el usuario

--- Proyecto/instalador.sh
+++ Proyecto/instalador.sh
@@ -3,6 +3,15 @@
 ### VARIABLES ###############################################################################################
 
 GRUPO=$PWD"/grupo3"
+EJECUTABLES_DIR="EJECUTABLES"             # 1) El directorio de ejecutables
+MAESTROS_TABLAS_DIR="MAESTROS"            # 2) El directorio de archivos maestros y tablas del sistema (aquí no van los datos de prueba!!)
+ARRIBOS_DIR="ARRIBOS"                     # 3) El directorio de arribo de archivos externos, es decir, los archivos que remiten las subsidiarias
+NOVEDADES_ACEPTADAS_DIR="NOVEDADES"       # 4) El directorio donde se depositan temporalmente las novedades aceptadas
+RECHAZADOS_DIR="RECHAZADOS"               # 5) El directorio donde se depositan todos los archivos rechazados
+PROCESADOS_DIR="PROCESADOS"               # 6) El directorio donde se depositan los archivos procesados 
+REPORTES_DIR="REPORTES"                   # 7) El directorio donde se depositan los reportes
+COMANDOS_LOGS_DIR="LOGS"                  # 8) El directorio donde se depositan los logs de los comandos 
+
 
 
 ### FUNCIONES ###############################################################################################
@@ -20,7 +29,7 @@ function checkPerlVersion
   fi
 }
 
-function createMainFolder
+function createMainDirectory
 {
   echo $GRUPO
   mkdir -p "$GRUPO"
@@ -33,6 +42,31 @@ function selectOption
   fi
 }
 
+function createSubDirectory
+{
+  echo $2
+  DIRECTORY=$1
+  read -r NEW_DIR
+  if [ ! "$NEW_DIR" == "" ]
+  then
+    DIRECTORY="$NEW_DIR"
+    break;
+  fi
+  mkdir -p "$DIRECTORY"
+}
+
+function createSubDirectories 
+{
+  createSubDirectory "$EJECUTABLES_DIR" "Por favor introduzca el directorio de ejecutables (Si presiona ENTER se creara el Default: $EJECUTABLES_DIR)"
+  createSubDirectory "$MAESTROS_TABLAS_DIR" "Por favor introduzca el directorio de archivos maestros y tablas (Si presiona ENTER se creara el Default: $MAESTROS_TABLAS_DIR)"
+  createSubDirectory "$ARRIBOS_DIR" "Por favor introduzca el directorio de los arribos (Si presiona ENTER se creara el Default: $ARRIBOS_DIR)"  
+  createSubDirectory "NOVEDADES_ACEPTADAS_DIR" "Por favor introduzca el directorio de novedades aceptadas (Si presiona ENTER se creara el Default: $NOVEDADES_ACEPTADAS_DIR)"
+  createSubDirectory "$RECHAZADOS_DIR" "Por favor introduzca el directorio de rechazados (Si presiona ENTER se creara el Default: $RECHAZADOS_DIR)"
+  createSubDirectory "$PROCESADOS_DIR" "Por favor introduzca el directorio de procesados (Si presiona ENTER se creara el Default: $PROCESADOS_DIR)"
+  createSubDirectory "$REPORTES_DIR" "Por favor introduzca el directorio de reportes (Si presiona ENTER se creara el Default: $REPORTES_DIR)"
+  createSubDirectory "$COMANDOS_LOGS_DIR" "Por favor introduzca el directorio de command logs (Si presiona ENTER se creara el Default: $COMANDOS_LOGS_DIR)"
+}
+
 ##############################################################################################################
 ### MAIN PROGRAM #############################################################################################
 
@@ -43,6 +77,7 @@ checkPerlVersion
     echo 'La version de Perl no es compatible (menor a la 5.0)'
   fi
 selectOption
-createMainFolder
+createMainDirectory
+createSubDirectories
 
 ##############################################################################################################
