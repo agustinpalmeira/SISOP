9ed782d47d37aa7f37701d24b9a823265535ce5a
Agustin Palmeira <agustin.palmeira@fdvsolutions.com>
2018-05-14 17:59:24 -0300

    - Function para mostrar los directorios creados

--- Proyecto/instalador.sh
+++ Proyecto/instalador.sh
@@ -15,8 +15,7 @@ COMANDOS_LOGS_DIR="LOGS"                  # 8) El directorio donde se depositan 
 ### FUNCIONES ###############################################################################################
 
 #Devuelve 0 en caso de que la version de Perl sea mayor o igual a la 5, 1 en caso contrario.
-function checkPerlVersion
-{
+function checkPerlVersion {
   if [ $(perl -V:version | grep "='[5-9]" -c) -eq 1 ] || [ $(perl -V:version | grep "='..\." -c) -eq 1] ;
   then
      echo 'La version de Perl es valida.'
@@ -27,8 +26,7 @@ function checkPerlVersion
   fi
 }
 
-function createMainDirectory
-{
+function createMainDirectory {
   echo $GRUPO
   mkdir -p "$GRUPO"
 }
@@ -40,8 +38,7 @@ function selectOption
   fi
 }
 
-function readSubDirectories 
-{
+function readSubDirectories {
   echo "Por favor introduzca el directorio de ejecutables (Si presiona ENTER se creara el Default: $EJECUTABLES_DIR)"
   read -r NEW_DIR
   if [ ! "$NEW_DIR" == "" ]
@@ -115,8 +112,7 @@ function readSubDirectories
   #readSubDirectory "$COMANDOS_LOGS_DIR" "Por favor introduzca el directorio de command logs (Si presiona ENTER se creara el Default: $COMANDOS_LOGS_DIR)"
 }
 
-function createSubDirectories 
-{
+function createSubDirectories {
     mkdir -p "$GRUPO/$EJECUTABLES_DIR"
     mkdir -p "$GRUPO/$MAESTROS_TABLAS_DIR"
     mkdir -p "$GRUPO/$ARRIBOS_DIR"
@@ -125,6 +121,20 @@ function createSubDirectories
     mkdir -p "$GRUPO/$PROCESADOS_DIR"
     mkdir -p "$GRUPO/$REPORTES_DIR"
     mkdir -p "$GRUPO/$COMANDOS_LOGS_DIR"
+}
+
+function showDirectoriesConfiguration {
+  echo "TP SO7508 Primer Cuatrimestre 2018. Tema O Copyright © Grupo 03"
+  echo "Librería del Sistema: dirconf"
+  echo "Ejecutables en: $GRUPO/$EJECUTABLES_DIR."
+  echo "Directorio para los archivos maestros: $GRUPO/$MAESTROS_TABLAS_DIR."
+  echo "Directorio para el arribo de archivos externos: $GRUPO/$ARRIBOS_DIR."
+  echo "Directorio para los archivos aceptados: $GRUPO/$NOVEDADES_ACEPTADAS_DIR."
+  echo "Directorio para los archivos rechazados: $GRUPO/$RECHAZADOS_DIR."
+  echo "Directorio para Archivos procesados: $GRUPO/$PROCESADOS_DIR."
+  echo "Directorio para los reportes: $GRUPO/$REPORTES_DIR." 
+  echo "Logs de auditoria del Sistema: $GRUPO/$COMANDOS_LOGS_DIR."
+  echo "Estado de la instalación: LISTA"
 }
 
 ##############################################################################################################
@@ -146,5 +156,6 @@ readSubDirectories
 
 createSubDirectories
 
+showDirectoriesConfiguration
 
 ##############################################################################################################
