71188c84bbc23c373cdf9cebd8b69db449849f44
Joaquin Blanco <joaquinbf@live.com>
2018-05-22 15:46:19 -0300

    arreglo el comando stopO.sh

--- Proyecto/stopO.sh
+++ Proyecto/stopO.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 #detectar si el demonio esta corriendo
 #HIPOTESIS: el demonio solo se ejecutara una vez.
-CANTIDAD_DEMONIOS=$(ps -e | pgrep -c "detecto.")
-if [$CANTIDAD_DEMONIOS -eq 1]; then
-  DEMONIO_PID=$(pgrep "detecto." | head -n 1)
+CANTIDAD_DEMONIOS=$(ps -e | pgrep -c "detectO.")
+if [ $CANTIDAD_DEMONIOS -eq 1 ]; then
+  DEMONIO_PID=$(pgrep "detectO." | head -n 1)
+  echo "El demonio detectO.sh con PID: $DEMONIO_PID ah sido detenido."
   kill -9 "$DEMONIO_PID"
   return 0
 fi
